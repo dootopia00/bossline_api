d22a71675ecba4a181e397c08d171957cf43c698
dooopia <dootopia00@gmail.com>
2022-04-17 23:02:58 +0900

    ㅁㅇㅇ

--- bossline_api/application/config/autoload.php
+++ bossline_api/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('s3', 'kinesisVideo', 'appPush','oldEncrypt','thumbnail','user_agent','sms','MintQuest', 'DialComm');
+$autoload['libraries'] = array('s3', 'oldEncrypt');
 
 /*
 | -------------------------------------------------------------------
